7ea94f3994069eceebaf02355588cf5767720138
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-12 01:01:38 -0300

    learning generics

---
+++ generics.ts
@@ -0,0 +1,19 @@
+class Queue<T> {
+  private data: T[] = [];
+
+  add(item: T) {
+    this.data.push(item);
+  }
+
+  remove() {
+    this.data.shift();
+  }
+}
+
+const nameQueue = new Queue<string>();
+nameQueue.add('luis');
+nameQueue.add('andrei');
+
+const numberQueue = new Queue<number>();
+numberQueue.add(10);
+numberQueue.add(10);
